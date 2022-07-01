398b52a42813904fd615b6c4f23fd47a25161bca
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2022-07-01 20:17:34 +0200

    fsk check as first typescript example

---
+++ TypeScript/src/fsk-check/0_start.vitest.ts
@@ -0,0 +1,47 @@
+import { expect, test } from 'vitest';
+
+type Movie = 'Wall·E' | 'Terminator' | 'Fight Club' | 'Sonic the Hedgehog' | 'Sonic the Hedgehog 2'
+
+const allowedToSee = (age: number, movie: Movie) => {
+  if (movie === 'Wall·E') {
+    return 'allowed'
+  }
+  if (movie === 'Sonic the Hedgehog' && age >= 6) {
+    return 'allowed'
+  }
+  if (movie === 'Sonic the Hedgehog 2' && age >= 12) {
+    return 'allowed'
+  }
+  if (movie === 'Terminator' && age >= 16) {
+    return 'allowed'
+  }
+  if (movie === 'Fight Club' && age >= 18) {
+    return 'allowed'
+  }
+  return 'denied'
+}
+
+
+test('everybody can see wall e', () => {
+  expect(allowedToSee(0, 'Wall·E')).toBe('allowed')
+})
+
+test('you have to be 16 to see sonic', () => {
+  expect(allowedToSee(5, 'Sonic the Hedgehog')).toBe('denied')
+  expect(allowedToSee(6, 'Sonic the Hedgehog')).toBe('allowed')
+})
+
+test('you have to be 16 to see sonic 2', () => {
+  expect(allowedToSee(11, 'Sonic the Hedgehog 2')).toBe('denied')
+  expect(allowedToSee(12, 'Sonic the Hedgehog 2')).toBe('allowed')
+})
+
+test('you have to be 16 to see terminator', () => {
+  expect(allowedToSee(15, 'Terminator')).toBe('denied')
+  expect(allowedToSee(16, 'Terminator')).toBe('allowed')
+})
+
+test('you have to be 18 to see fight club', () => {
+  expect(allowedToSee(17, 'Fight Club')).toBe('denied')
+  expect(allowedToSee(18, 'Fight Club')).toBe('allowed')
+})
---
+++ TypeScript/src/fsk-check/1_step.vitest.ts
@@ -0,0 +1,69 @@
+import { expect, test } from 'vitest';
+
+type MovieTitle = 'Wall·E' | 'Terminator' | 'Fight Club' | 'Sonic the Hedgehog' | 'Sonic the Hedgehog 2'
+
+type Movie = {
+  title: MovieTitle
+  minimumAge: number
+}
+
+const allowedToSeeThis = (movie: Movie, age: number) => {
+  if (age >= movie.minimumAge) {
+    return 'allowed'
+  }
+  return 'denied'
+}
+
+
+test('everybody can see wall e', () => {
+  expect(allowedToSee(wallE(), atTheAgeOf(0))).toBe('allowed')
+})
+
+test('you have to be 16 to see sonic', () => {
+  expect(allowedToSee(sonicTheHedgehog(), atTheAgeOf(5))).toBe('denied')
+  expect(allowedToSee(sonicTheHedgehog(), atTheAgeOf(6))).toBe('allowed')
+})
+
+test('you have to be 16 to see sonic 2', () => {
+  expect(allowedToSee(sonicTheHedgehog2(), atTheAgeOf(11))).toBe('denied')
+  expect(allowedToSee(sonicTheHedgehog2(), atTheAgeOf(12))).toBe('allowed')
+})
+
+test('you have to be 16 to see terminator', () => {
+  expect(allowedToSee(terminator(), atTheAgeOf(15))).toBe('denied')
+  expect(allowedToSee(terminator(), atTheAgeOf(16))).toBe('allowed')
+})
+
+test('you have to be 18 to see fight club', () => {
+  expect(allowedToSee(fightClub(), atTheAgeOf(17))).toBe('denied')
+  expect(allowedToSee(fightClub(), atTheAgeOf(18))).toBe('allowed')
+})
+
+const atTheAgeOf = (age: number) => age
+
+const allowedToSee = (movie: Movie, age: number) => allowedToSeeThis(movie, age)
+
+const wallE = (): Movie => ({
+  title: 'Wall·E',
+  minimumAge: 0,
+})
+
+const sonicTheHedgehog = (): Movie => ({
+  title: 'Sonic the Hedgehog',
+  minimumAge: 6,
+})
+
+const sonicTheHedgehog2 = (): Movie => ({
+  title: 'Sonic the Hedgehog 2',
+  minimumAge: 12,
+})
+
+const terminator = (): Movie => ({
+  title: 'Terminator',
+  minimumAge: 16,
+})
+
+const fightClub = (): Movie => ({
+  title: 'Fight Club',
+  minimumAge: 18,
+})
