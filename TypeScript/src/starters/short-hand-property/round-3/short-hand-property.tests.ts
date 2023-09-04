4ab5a07e2411ba0d0faec0b112f3f88dcdde83f9
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2023-09-04 22:22:30 +0200

    round 3

---
+++ TypeScript/src/starters/short-hand-property/round-3/short-hand-property.tests.ts
@@ -0,0 +1,20 @@
+import { expect, test } from 'vitest'
+import { anyCoordinates, anyLocation } from './short-hand-property.js'
+
+test('default to the eifel tower', () => {
+  const { coordinates } = anyLocation(anyCoordinates())
+  expect(coordinates.latitude).toEqual(48.858372)
+  expect(coordinates.longitude).toEqual(2.294481)
+})
+
+test('override latitude', () => {
+  const { coordinates } = anyLocation(anyCoordinates({ latitude: 13.412469 }))
+  expect(coordinates.latitude).toEqual(13.412469)
+  expect(coordinates.longitude).toEqual(2.294481)
+})
+
+test('override longitude', () => {
+  const { coordinates } = anyLocation(anyCoordinates({ longitude: 103.866989 }))
+  expect(coordinates.latitude).toEqual(48.858372)
+  expect(coordinates.longitude).toEqual(103.866989)
+})
---
+++ TypeScript/src/starters/short-hand-property/round-3/short-hand-property.ts
@@ -0,0 +1,16 @@
+type Coordinates = { latitude: number; longitude: number }
+
+const eifelTower = () => ({ latitude: 48.858372, longitude: 2.294481 })
+
+export const anyCoordinates = (overrides: Partial<Coordinates> = {}) => {
+  return {
+    ...eifelTower(),
+    ...overrides,
+  }
+}
+
+export const anyLocation = (coordinates: Coordinates = anyCoordinates()) => {
+  return {
+    coordinates,
+  }
+}
