d8a0544904f3be56e55f6aaecc826abd98691f70
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2023-09-19 22:02:45 +0200

    extract ensureEmail helpder

--- TypeScript/src/birthday-greetings/fakes/email/chai-email-from.ts
+++ TypeScript/src/birthday-greetings/fakes/email/chai-email-from.ts
@@ -1,6 +1,7 @@
-import { isEmail } from './smtp-server.js'
+import { Email, isEmail } from './smtp-server.js'
 import { AssertionError } from 'chai'
 import { ChaiPlugin } from './chai-email.js'
+import ChaiUtils = Chai.ChaiUtils
 
 declare global {
   namespace Chai {
@@ -14,24 +15,20 @@ declare global {
   }
 }
 
+function ensureEmail(chai: Chai.AssertionStatic, utils: ChaiUtils, actual: any): asserts actual is Email {
+  const negated = utils.flag(chai, 'negate') as boolean
+  if (!isEmail(actual)) {
+    chai.assert(negated, 'expected an Email but got #{act}', 'expected an Email but got #{act}', 'Email', actual, false)
+  }
+}
+
 export const ChaiEmailFrom: ChaiPlugin = function (chai, utils) {
   const Assertion = chai.Assertion
   Assertion.addMethod('from', function (expectedFrom: string, message?: string) {
     const actual = this._obj
     const ssfi = utils.flag(this, 'ssfi')
-    const negated = utils.flag(this, 'negate') as boolean
 
-    if (!isEmail(actual)) {
-      this.assert(
-        negated,
-        'expected an Email but got #{act}',
-        'expected an Email but got #{act}',
-        'Email',
-        actual,
-        false,
-      )
-      return
-    }
+    ensureEmail(this, utils, actual)
 
     const from = actual.from
     if (from === undefined) {
